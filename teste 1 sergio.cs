01638f8fb4bfb59d26f3fd3f7debda070d21e91d
Lana Cassou <lanacassou@bne.com.br>
2026-06-22 15:13:53 -0300

    TAREFA UM, TESTES 1,2 E 3

--- teste 1 sergio.cs
+++ teste 1 sergio.cs
@@ -1,4 +1,11 @@
-﻿using System;
+TAREFA 1
+
+
+    
+TESTE UM
+
+
+using System;
 
 namespace teste1
 {
@@ -21,3 +28,50 @@ Console.WriteLine(v[3]);
 }
 }
 
+
+TESTE DOIS 
+
+
+
+using System;
+
+int[] v = new int[6];
+int a = 2;
+
+while (a < 6)
+{
+    v[a] = a * 10;
+    a++;
+}
+
+Console.WriteLine(v[0]);
+Console.WriteLine(v[1]);
+Console.WriteLine(v[2]);
+Console.WriteLine(v[3]);
+Console.WriteLine(v[4]);
+Console.WriteLine(v[5]);
+
+
+
+TESTE TRÊS
+
+
+using System;
+
+int[] v = new int[6];
+int a = 7;
+int b = 0; 
+
+while (b < 3) 
+{
+   
+    v[b] = 8 + (b * 2); 
+    b = b + 1;
+}
+
+Console.WriteLine(v[0]); 
+Console.WriteLine(v[1]); 
+Console.WriteLine(v[2]); 
+Console.WriteLine(v[3]); 
+Console.WriteLine(v[4]); 
+Console.WriteLine(v[5]); 
