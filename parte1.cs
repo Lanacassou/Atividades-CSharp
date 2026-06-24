c7e5a9fc14b09ee389aa88e50271894155618242
Lana Cassou <lanacassou@bne.com.br>
2026-06-24 14:39:47 -0300

    PROBLEMA OITO

--- parte1.cs
+++ parte1.cs
@@ -1,53 +1,50 @@
-using System;
+PROBLEMA 8
 
-class Program
+sing System;
+
+class Tabela03
 {
     static void Main()
     {
-        
-        Investimento inv = new Investimento(1000.00m, 3.0m, 8, 10);
-        inv.ExibirTabela();
+        Problema8();
+    }
+
+    static void Problema8()
+    {
+        double valorInvestido;
+        double taxaJuros;
+        int periodoMeses;
+        double resgate;
+
+        Console.Write("Valor Investido: ");
+        valorInvestido = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Taxa de Juros (%): ");
+        taxaJuros = Convert.ToDouble(Console.ReadLine()) / 100;
+
+        Console.Write("Período (meses): ");
+        periodoMeses = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Valor do Resgate: ");
+        resgate = Convert.ToDouble(Console.ReadLine());
+
+        double rendimento =
+            valorInvestido * Math.Pow(1 + taxaJuros, periodoMeses);
+
+        double saldoLiquido = rendimento - resgate;
+
+        Console.WriteLine();
+        Console.WriteLine("----------------------------------------------------------------");
+        Console.WriteLine("Valor Investido\tTaxa\tRendimento\tPeríodo\tResgate\tSaldo Líquido");
+        Console.WriteLine("----------------------------------------------------------------");
+
+        Console.WriteLine(
+            "R$ " + valorInvestido.ToString("F2") + "\t" +
+            (taxaJuros * 100).ToString("F2") + "%\t" +
+            "R$ " + rendimento.ToString("F2") + "\t" +
+            periodoMeses + "\t" +
+            "R$ " + resgate.ToString("F2") + "\t" +
+            "R$ " + saldoLiquido.ToString("F2")
+        );
     }
 }
-
-class Investimento
-{
-    public decimal valorPresente { get; set; }
-    public decimal taxaJuros { get; set; }
-    public int meses { get; set; }
-    public int dias { get; set; }
-
-    public Investimento(decimal vp, decimal taxa, int m, int d)
-    {
-        valorPresente = vp;
-        taxaJuros = taxa;
-        meses = m;
-        dias = d;
-    }
-
-    public void ExibirTabela()
-    {
-        decimal taxaDecimal = taxaJuros / 100m;
-        decimal saldo = valorPresente; 
-
-        Console.WriteLine("Valor Presente | Juros | Meses | Valor Futuro");
-        Console.WriteLine("---------------------------------------------");
-
-
-        for (int i = 1; i <= meses; i++)
-        {
-            decimal jurosDoMes = saldo * taxaDecimal;
-            saldo = Math.Round(saldo + jurosDoMes, 2); 
-
-            Console.WriteLine($"R$ {valorPresente:N2}    | {taxaJuros}%   | {i}     | R$ {saldo:N2}");
-        }
-
-        
-        decimal jurosDias = saldo * (taxaDecimal / 30m) * dias;
-        decimal saldoFinal = Math.Round(saldo + jurosDias, 2) - 0.26m; 
-
-        Console.WriteLine($"R$ {valorPresente:N2}    | {taxaJuros}%   | 8,33  | R$ {saldoFinal:N2}");
-        Console.WriteLine("---------------------------------------------");
-    }
-}
-
